7bb1526239b464b801fe5f2c452bd669f7be42b6
alexsan-s <64074234+alexsan-s@users.noreply.github.com>
2020-07-26 20:34:27 -0300

    quicksort

---
+++ Chapter 4/quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivo = array[0]
+        menores = [i for i in array[1:] if i <= pivo]
+        maiores = [i for i in array[1:] if i > pivo]
+        return quicksort(menores) + [pivo] + quicksort(maiores)
+
+
+print(quicksort([18, 5, 2, 3]))
